8b1f3745bdbc1f17a34fe4542abfcb2c955903c2
tommie1236 <90513578+Tommie1236@users.noreply.github.com>
2024-05-22 22:44:21 +0200

    add gitignore and update main.cpp

--- src/firmware/main.cpp
+++ src/firmware/main.cpp
@@ -1,11 +1,21 @@
-#include <pico/stdlib.h>
+#include <hardware/gpio.h>
+#include <pico/time.h>
 #include <stdio.h>
 
 
 int main () {
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(1000);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(1000);
+    };
+    return 0;
 };
-
 
 class MFader {
     private:
@@ -41,4 +51,6 @@ int UpdateFaderPosition(MFader fader, int value) {
         fader.moveDown();
     };
     fader.moveStop();
+    
+    return faderPos;
 }
